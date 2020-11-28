96efc1a0fad110a7a8772304e99edc8b92a2821d
michael <mpark55@asu.edu>
2020-11-28 03:06:26 -0800

    Finally, dictionary is good with core data

--- collegeBudgetApp/TransactionList.swift
+++ collegeBudgetApp/TransactionList.swift
@@ -17,118 +17,29 @@ class TransactionList {
     
     // a list of tuples, just for ease of grabbing the right transactions because there will be a search function
     
-    var list: [(name: String, trans: Transaction)] = [(String, Transaction)]()
+    var TransactionList:[Transaction] = []
     
-    /**
-            Adds a new transaction to the list
-                - Parameters:
-                    -newTransaction: the transaction we are adding
-     
-     */
-    func add(_ newTrans: Transaction) {
-        list.append((newTrans.getID(), newTrans))
-        numOfTrans += 1
+    init() {
+        
     }
     
-    /**
-    Deletes the transaction currently being viewed
-    */
-    func delete() -> Bool {
-        if (numOfTrans != 0) {
-            list.remove(at: currentTrans)
-            numOfTrans -= 1
-            return true
-        }
-        return false
+    
+    func getCount() -> Int {
+        return TransactionList.count
     }
     
-    /**
-    Iterates to the next transaction available, if it exists
-    
-    - Returns:
-        - an optional transaction that comes after the current column
-            being viewed
-    */
-    func getNext() -> Transaction? {
-        if (currentTrans >= (numOfTrans - 1)) {
-            return nil
-        }
-        else {
-            currentTrans += 1
-            return list[currentTrans].trans
-        }
+    func getTransObject(item:Int) -> Transaction {
+        return TransactionList[item]
     }
     
-    /**
-    Iterates to the previous column available, if it exists
-    
-    - Returns:
-        - an optional transaction that comes before the current transaction
-            being viewed
-    */
-    func getPrevious() -> Transaction? {
-        if (currentTrans <= 0) {
-            return nil
-        }
-        else {
-            currentTrans -= 1
-            return list[currentTrans].trans
-        }
+    func removeTransObject(item:Int) {
+        TransactionList.remove(at: item)
     }
     
-    /**
-    A convenience function to grab the first transaction in the list, if it exists
-    
-    - Returns:
-        - an optional transaction that is the first in the list
-    */
-    func getFirst() -> Transaction? {
-        if let first = list.first {
-            currentTrans = 0
-            return first.trans
-        } else {
-            return nil
-        }
+    func addTransObject(category:String, title: String, date: String, amount: Double, photo: Data, Id: String) -> Transaction{
+        let t = Transaction(t_cg: category, t_tl: title, t_dt: date, t_dAm: amount, t_pt: photo, t_Id: Id)
+        TransactionList.append(t)
+        return t
     }
     
-    /**
-    Searches for the Transactino with the give ID
-    
-     - Parameters:
-        - toFind: the ID of the Transaction being searched for
-     
-    - Returns:
-        - an optional Transaction that matches the parameter building name
-    */
-    func search(id toFind: String) -> Transaction? {
-        for i in list.indices {
-            if (list[i].name == toFind) {
-                currentTrans = i
-                return list[i].trans
-            }
-        }
-        return nil //didn't find it
-    }
-    
-    /**
-    Convenience function to print all of the transactions in the list
-    */
-    func printColumns() {
-        for item in list {
-            print("\(item.name.uppercased()):")
-            print("Category: \(item.trans.getCategory())")
-            print("Date: \(item.trans.getDate())")
-            print("Dollar Amount: \(item.trans.getDollarAmount())")
-            print("Trans ID: \(item.trans.getID())")
-        }
-    }
-    
-    /**
-    Removes all transactions in the list and resets the currentTransaction index
-    */
-    func removeAll() {
-        currentTrans = 0
-        numOfTrans = 0
-        list.removeAll()
-    }
 }
--- collegeBudgetApp/ViewController.swift
+++ collegeBudgetApp/ViewController.swift
@@ -11,12 +11,47 @@ import CoreData
 
 class ViewController: UIViewController {
     
+    var transactionList: TransactionList = TransactionList()
+    
     
     @IBOutlet weak var dailyAmount: UILabel!
+    
+    
+    let managedObjectContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var fetchedResults = [TransactionEntity]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        fetchCoreDataEntities()
+    }
+    
+    /**
+    Grabs the ColumnEntities from the managed object context, and updates the Column List to
+     reflect them.
+    */
+    func fetchCoreDataEntities() {
+        // create a fetch request for all of our ColumnEntities
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "TransactionEntity")
+        
+        // send the fetch request to the managedObjectContext and grab
+        // the results as a list of ColumnEntities
+        fetchedResults = ((try? managedObjectContext.fetch(fetchRequest)) as? [TransactionEntity])!
+        
+        // clear out all of the columns we have just in case we are holding
+        // on to old ones
+        //transactionList.removeAll()
+        
+        // for each column that we have in the core data store, add a column to our list
+        /*
+        for entity in fetchedResults {
+            if let building = entity.transID, let type = entity.transPhoto {
+                transactionList.add(Transaction(category: String, title: String, date: String, dollarAmount: Double, photo: Data, Id: String))
+            }
+            
+        }
+        */
     }
 
 
